f7d51fc2c9d85a65b99cefe035008304817605a3
Logon Aluno <logonrmlocal@fiap.com.br>
2023-09-29 10:56:41 -0300

    Subindo o CheckPoint 1 de JSt

---
+++ script.js
@@ -0,0 +1,52 @@
+
+//  No arquivo script.js, criar uma lista de variáveis com os seguintes nomes:
+
+    //  nome que recebe o seu nome
+    let nome = "Gilson";
+
+    //  idade que recebe a sua idade
+    let idade = 19;
+
+    // status com um valor booleano verdadeiro
+    let status = true;
+
+    // valorProduto que recebe um valor número flutuante, com o preço de 10.50
+    let valorProduto = 10.50;
+
+    // listaTimes com uma Array com três times.
+    let listaTimes = ["São Paulo", "Rio de Janeiro", "Bahia"];
+
+
+// 6 - Listar cada variável em um console.log
+
+console.log(nome);
+console.log(idade);
+console.log(status);
+console.log(valorProduto);
+console.log(listaTimes);
+
+// 7 - No mesmo arquivo, criar operações matemáticas com 4 variáveis:
+// - soma
+// - subtracao
+// - multiplicacao
+// - divisao
+
+let num1 = 10;
+let num2 = 5;
+
+let soma = 0;
+let subtracao = 0;
+let multiplicacao = 0;
+let divisao = 0;
+
+soma = num1 + num2;
+subtracao = num1 - num2;
+multiplicacao = num1 * num2;
+divisao = num1 / num2;
+
+// 8 - Mostrar os resultados no console.log
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
